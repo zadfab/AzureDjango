7770b5ccf6b86a65f348505a5dedddb96e95293b
zadfab <zaidmohd466@gmail.com>
2022-01-05 20:54:06 +0530

    Deployment

---
+++ Nid/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class NidConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Nid'
---
+++ Nid/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def welcome(request):
+    print(request)
+    return HttpResponse("<h1>Deployment Completed!</h1>")
